2bb7291bc94b3f5bb105a6f9774b680d524a575b
Patrick Bonayon <bonayonpatrick@example.com>
2022-08-06 20:31:55 +0800

    clean up code

---
+++ app/app.py
@@ -0,0 +1,50 @@
+from flask import Flask, send_from_directory, request
+
+from webcam_recorder import WebcamRecorder
+
+app = Flask(__name__)
+wcd_rec = WebcamRecorder(users=[
+    "ohbabyy_"
+])
+
+
+@app.route("/get_users")
+def get_users():
+    return wcd_rec.get_users()
+
+
+@app.route("/get_recordings")
+def get_recordings():
+    return wcd_rec.get_files()
+
+
+@app.route("/delete_file")
+def delete_file():
+    wcd_rec.logger.debug("delete_file called")
+    args = request.args
+
+    if args.get("path"):
+        wcd_rec.delete_file(args.get("path"))
+        return "success"
+
+    return "fail"
+
+
+@app.route('/download_file')
+def download_file():
+    wcd_rec.logger.debug("download_file called")
+    args = request.args
+
+    if (not args.get("dir")) and (not args.get("name")):
+        return "missing parameters"
+
+    try:
+        return send_from_directory(directory=args["dir"], path=args["name"], as_attachment=True)
+    except Exception as e:
+        return f"{e}"
+
+
+@app.route("/")
+def index():
+    wcd_rec.logger.debug("index called")
+    return f"{wcd_rec.app_name}: there's nothing in here"
